de0ca4ba7be8b384384031cc33c881ba54e9dbf4
ofwebdev <nkomorfaruk@gmail.com>
2023-05-03 02:39:37 +0600

    Add findCategoryById now I have only two data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const category = require("./data/category.json");
+const data = require("./data/data.json");
 
 const app = express();
 app.use(cors());
@@ -14,6 +15,25 @@ app.get("/", (req, res) => {
 
 app.get("/country", (req, res) => {
   res.send(category);
+});
+
+app.get("/country/:id", (req, res) => {
+  const id = req.params.id;
+
+  const findCategoryById = data.filter((getData) => getData.category_id === id);
+
+  if (id == 0) {
+    res.send(data);
+  } else {
+    if (findCategoryById.length == 0) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Invalid ID parameter",
+      });
+    }
+  }
+
+  res.send(findCategoryById);
 });
 
 app.listen(port, () => {
