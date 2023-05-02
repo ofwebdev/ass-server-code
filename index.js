414aa3ed22c54ed84e2a4a086208dac7a03d14fb
ofwebdev <nkomorfaruk@gmail.com>
2023-05-03 02:25:38 +0600

    Json data add and get country category api

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const category = require("./data/category.json");
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello server");
+});
+
+app.get("/country", (req, res) => {
+  res.send(category);
 });
 
 app.listen(port, () => {
