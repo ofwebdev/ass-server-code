e67a36d73cfd5a24b2a735247a742c01b33ba171
ofwebdev <nkomorfaruk@gmail.com>
2023-05-04 11:00:55 +0600

    Add blog api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const category = require("./data/category.json");
 const data = require("./data/data.json");
+const blog = require("./data/blog.json");
 
 const app = express();
 app.use(cors());
@@ -52,6 +53,28 @@ app.get("/recipe/:id", (req, res) => {
   }
 
   res.send(findRecipeById);
+});
+
+app.get("/blog", (req, res) => {
+  res.send(blog);
+});
+
+app.get("/blog/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const findBlogById = blog.find((blogId) => blogId.id === parseInt(id));
+    if (!findBlogById) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Invalid ID parameter",
+      });
+    }
+
+    res.send(findBlogById);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(port, () => {
