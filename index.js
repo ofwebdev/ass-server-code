c3dba276eff471454e3ac1d1a0f7b1564a5cd159
ofwebdev <nkomorfaruk@gmail.com>
2023-05-26 02:32:00 +0600

    Update json data

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const category = require("./data/category.json");
 const data = require("./data/data.json");
 const blog = require("./data/blog.json");
 
@@ -12,29 +11,6 @@ const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello server");
-});
-
-app.get("/country", (req, res) => {
-  res.send(category);
-});
-
-app.get("/country/:id", (req, res) => {
-  const id = req.params.id;
-
-  const findCategoryById = data.filter((getData) => getData.category_id === id);
-
-  if (id == 0) {
-    res.send(data);
-  } else {
-    if (findCategoryById.length == 0) {
-      return res.status(404).json({
-        status: "fail",
-        message: "Invalid ID parameter",
-      });
-    }
-  }
-
-  res.send(findCategoryById);
 });
 
 app.get("/recipe", (req, res) => {
